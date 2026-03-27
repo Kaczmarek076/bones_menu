23a4d13feab9e888fa55ef40061b330d2d971855
Kaczmarek076 <sebkac01@easv.dk>
2026-03-27 10:43:51 +0100

    lkjdf

--- src/main/java/org/example/bones_menu/HelloApplication.java
+++ src/main/java/org/example/bones_menu/HelloApplication.java
@@ -1,19 +1,34 @@
 package org.example.bones_menu;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.*;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.*;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        AnchorPane bonesMenu = new AnchorPane();
+        GridPane menu = new GridPane();
+        Scene scene = new Scene(bonesMenu, 500, 500);
+        Label label = new Label();
+
+        stage.setTitle("Bones!");
         stage.setScene(scene);
         stage.show();
+        Label titel1 = new Label("Menu");
+
+
+        // Gridpane
+        menu.setHgap(7);
+        menu.setVgap(15);
+
+        bonesMenu.getChildren().addAll(menu);
+
     }
 }
+    }
+            }
